4d941e9327c7b27fc1308d3e2abc6a37a77400a7
Aaron Suman <oneajs@gmail.com>
2020-04-06 20:53:29 +0100

    Remove comments in sketch

--- sketch.py
+++ sketch.py
@@ -12,16 +12,3 @@ class Sketch():
             for support in arg.supports:
                 dot.edge(arg.name, support.name, label="+", arrowhead="onormal")
         dot.render("sketch/"+name, view=show)
-
-    # def draw(self, name, framework, show):
-    #     try:
-    #         dot = Digraph(comment=name, format='svg')
-    #         for arg in framework.arguments:
-    #             dot.node(arg.name, arg.name.upper(), fontsize="22", shape="circle")
-    #             for attack in arg.attacks:
-    #                 dot.edge(arg.name, attack.name, arrowhead="normal", constraint="true")
-    #             for support in arg.supports:
-    #                 dot.edge(arg.name, support.name, arrowhead="onormal", constraint="true")
-    #         dot.render("sketch/sbaeval", view=False)
-    #     except:
-    #         print("Error generating sketch.")
